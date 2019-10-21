c01973de274636d4b47ad5bd8ce32176146d58b8
Tim Galloway <galloway@iinet.net.au>
2019-10-21 15:36:33 +0800

    Added a writeline to the report command for a better layout

--- RobotSimulation/Program.cs
+++ RobotSimulation/Program.cs
@@ -51,6 +51,7 @@ namespace RobotSimulation
                 }
                 if ((line == "REPORT") && robotPosition.Valid)
                 {
+                    Console.WriteLine(""); 
                     ConsoleRenderer.RenderDocument(robotPosition.Report());
                 }
 
